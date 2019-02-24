934a724ebfc4b2cf7c1bedbe5fd7f638542bd5cf
mana.guru@gmail.com <mana.guru@gmail.com>
2019-02-23 21:53:52 -0700

    Final Version

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,12 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
+const HDWallet = require('truffle-hdwallet-provider');
 //
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraKey = fs.readFileSync(".ifurasecret").toString().trim();
+
 
 module.exports = {
   /**
@@ -58,7 +59,12 @@ module.exports = {
       network_id: "*",       // Any network (default: none)
     },
 
-
+    rinkeby: {
+      provider: () => new HDWallet(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
+    },
     // Another network with more advanced options...
     // advanced: {
       // port: 8777,             // Custom port
